a5d933f5f80d748dad1303146e30a0a9613a4b7c
Hamsa <hamsamuse.hm@gmail.com>
2024-09-12 12:22:32 +0100

    123

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get("/123", (req, res) => {
-  res.send("Get off the page, you are about to be hit with the biggest virus in the world ");
+  res.send("Get off the page, you are about to be hit with the biggest virus in the worl");
 });
 
 
