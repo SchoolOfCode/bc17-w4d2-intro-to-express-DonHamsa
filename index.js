88da4a6e38cdbcd7685e9588159fed9db1f9d19a
Hamsa <hamsamuse.hm@gmail.com>
2024-09-12 11:32:24 +0100

    123

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from 'express'
+
+const app= express();
+const port= 3000 ;
+
+app.get('/donabdi', (req,res)=>{
+    res.send('Get off the page, you are about to be hit with the biggest virus')
+})
+
+
+
+app.listen(port, ()=> `Example app listening on port ${port}` )
+
+
+
+
