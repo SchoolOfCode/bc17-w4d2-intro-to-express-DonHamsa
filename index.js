59d9ec4e9ba5c25f1557915dafce56a9728ced65
Hamsa <hamsamuse.hm@gmail.com>
2024-09-12 11:33:21 +0100

    123

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from 'express'
 const app= express();
 const port= 3000 ;
 
-app.get('/donabdi', (req,res)=>{
+app.scget('/donabdi', (req,res)=>{
     res.send('Get off the page, you are about to be hit with the biggest virus')
 })
 
